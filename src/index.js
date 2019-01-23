e2ce89c3629e50d40955b00014332be7919931f6
Mkpo-uto Edet <mkpoutoe@hotmail.com>
2019-01-24 10:52:45 +1100

    Update state properties not directly but with set state !!!

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ class App extends React.Component {
     constructor(props){
         super(props);
         //Initializes state object and assigns a value of null to it's property "latitude"
+        //THIS IS THE ONLY TIME a direct assignment to this.state can be done
         this.state = { lat: null };
+        //Retrieve current location when state is initialized
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //take attribute latitude out of position object to update lat value 
+                //by calling setState!!!
+                this.setState({ lat:position.coords.latitude});
+            },
+            err => console.log(err)
+        );
     }
 
 
     //  React requires definition of render method
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
+        
 
         return <div>Latitude: {this.state.lat}</div>
     }
