76adfe9837aee947701dc1a995cb598b47306223
Mkpo-uto Edet <mkpoutoe@hotmail.com>
2019-01-24 10:43:19 +1100

    Created a constructor method to initialize State

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        //Initializes state object and assigns a value of null to it's property "latitude"
+        this.state = { lat: null };
+    }
+
+
+    //  React requires definition of render method
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
             err => console.log(err)
         );
 
-        return <div>Latitude: </div>
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
