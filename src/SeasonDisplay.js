d182a4bb84226bac3957547ade51dada8792b11d
Mkpo-uto Edet <mkpoutoe@hotmail.com>
2019-01-29 10:16:30 +1100

    Dispaly message for seasons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 const getSeason = (lat, month) => { 
     if(month > 2 && month < 9){
-    return lat > 0 ? 'summer' : 'winter';
+        return lat < 0 ? 'summer' : 'winter';
     } else {
-        return lat < 0 ? 'winter' : 'summer';
+        return lat > 0 ? 'winter' : 'summer';
     }
 }
 
 const SeasonDisplay = props => {
 
     const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
 
-    console.log(season);
+    return <div>
+        <h1>{text}</h1>
+    </div>
+   // console.log(season);
 
-    return <div>Season Display</div>
+
+   //return <div>Season Display</div>
 };
 
 export default SeasonDisplay;
