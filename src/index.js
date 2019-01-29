d3c10a84b2e842e58d1bca2c66035d34eab70719
Mkpo-uto Edet <mkpoutoe@hotmail.com>
2019-01-29 11:03:35 +1100

    Modified initial display page for first time user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     // constructor(props){
@@ -38,7 +39,7 @@ class App extends React.Component {
             //<div>Latitude: {this.state.lat}</div>
         }
 
-        return <div>Loading!</div>
+        return < Spinner />
     }
 }
 
